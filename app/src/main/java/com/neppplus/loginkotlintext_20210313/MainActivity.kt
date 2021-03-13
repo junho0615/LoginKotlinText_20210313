ff8eeb1a7955940880f740114865d2c3fd2e850f
홍준호 <0615wnsgh@gmail.com>
2021-03-13 11:29:44 +0900

    입력한 아이디/비번을 추출까지

--- app/src/main/java/com/neppplus/loginkotlintext_20210313/MainActivity.kt
+++ app/src/main/java/com/neppplus/loginkotlintext_20210313/MainActivity.kt
@@ -2,10 +2,27 @@ package com.neppplus.loginkotlintext_20210313
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginBtn.setOnClickListener {
+
+//            그 안에서
+//            1. 아이디 / 비번에 적힌 값 => 변수 2개로 저장
+            val inputId = idEdt.text.toString()
+            val inputPw = pwEdt.text.toString()
+
+
+
+
+            }
+
+
+        }
+
     }
 }
